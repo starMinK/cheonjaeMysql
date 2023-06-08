e1cca97a18dd62210a3c472745470b0c9a5af489
starMinK <rbals040329@gmail.com>
2023-06-08 17:47:15 +0900

    Create MemberTotalMethod.java

---
+++ src/lecture0608/MemberTotalMethod.java
@@ -0,0 +1,13 @@
+package lecture0608;
+/*문제: 회원관리 시스템을 업그레이드 하여 각각의 method를 이용한 객체지향 프로그래밍을 작성
+* 1. 회원 정보 조회
+* 2. 회원 정보 입력
+* 3. 회원 정보 수정
+* 4. 회원 정보 삭제
+* 5. 회원 정보 종료
+* 위 5가지의 시스템을 가진 프로그램을 작성하라.*/
+public class MemberTotalMethod {
+    public static void main(String[] args) {
+        
+    }
+}
