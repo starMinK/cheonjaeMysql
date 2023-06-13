371046c5be7571edc13fcae464278cb760ebf2fe
starMinK <rbals040329@gmail.com>
2023-06-13 17:49:28 +0900

    commit

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,21 @@
 
 X
 (src/lecture0608/PackageMemberInsert.java,5\3\53187b01dc0b6200e38a12857e3e5cb3c280a641
+P
+ src/lecture0609/ScoreDelete.java,b\b\bb78b0efa0f191e5e81fa1c9d32cc639e0e7bd66
+X
+(src/lecture0608/PackageMemberUpdate.java,3\e\3eb6cbc590fc8e57158bbeb3a23589f1d094ef44
+P
+ src/lecture0609/ScoreUpdate.java,9\b\9bb33e48bb4985b487801ba360455dfc83c34de4
+P
+ src/lecture0609/ScoreSelect.java,f\d\fd09ce717025f372e6891d18d20d2387ff268208
+X
+(src/lecture0608/PackageMemberDelete.java,a\7\a7ae65786b007434138cabbd36b3afbf4c9c2b93
+R
+"src/lecture0608/PackageMember.java,a\b\abc48b8127e6b8a58c1996ff5ff7ed66d9572290
+V
+&src/lecture0612/OracleMemberTotal.java,4\a\4ac3ccd7fbc1dff759e48091de8eff877b27fb1e
+V
+&src/lecture0608/MemberTotalMethod.java,8\e\8eaf63a1ee623a9bec7fae568a46db5ae5cafa07
+Q
+!src/lecture0612/OracleMember.java,0\3\0364754c0308bb94cbf9f90a06bc9789fbc5ae15
--- src/lecture0612/OracleMemberQuestion.java
+++ src/lecture0612/OracleMemberQuestion.java
@@ -1,67 +1,180 @@
 package lecture0612;
 
-import lecture0609.ScoreTotalMethod;
-
 import java.sql.*;
 import java.util.Scanner;
 
 public class OracleMemberQuestion {
-//    memberInterface memberOverload = new MemberOverload();
-    private static Connection conn;
-    private static PreparedStatement prestate;
-    private static ResultSet resultSet;
+    //    memberInterface memberOverload = new MemberOverload();
+    protected static final String DB_URL = "jdbc:mysql://localhost:3306/mindb";
+    protected static final String DB_USER = "root";
+    protected static final String DB_PASSWORD = "1234";
 
-    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        ScoreTotalMethod scoreTotalMethod = new ScoreTotalMethod();
+        MemberDAO memberDAO = new MemberDAOImpl();
+        Scanner scanner = new Scanner(System.in);
 
-        String driver = "com.mysql.cj.jdbc.Driver";
-        String url = "jdbc:mysql://localhost:3306/member";
-        String userId = "testuser";
-        String userPassword = "1234";
+        while (true) {
+            System.out.println("1. 회원 추가");
+            System.out.println("2. 회원 수정");
+            System.out.println("3. 회원 조회");
+            System.out.println("4. 회원 삭제");
+            System.out.println("5. 프로그램 종료");
+            System.out.print("메뉴 선택: ");
 
-        try {
-            Class.forName(driver);
-            conn = DriverManager.getConnection(url, userId, userPassword);
-        } catch (ClassNotFoundException e) {
-            System.out.println("db can't load");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("db can't connect");
-        }
-        System.out.println("choose menu");
-        int choose = sc.nextInt();
+            int choice = Integer.parseInt(scanner.nextLine());
 
-        switch (choose) {
-            case 1:
-                memberInsert("asd", "asd", 0.0, 0.0, 1);
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + choose);
+            switch (choice) {
+                case 1:
+                    memberDAO.addMember();
+                    break;
+                case 2:
+                    memberDAO.updateMember();
+                    break;
+                case 3:
+                    memberDAO.getMember();
+                    break;
+                case 4:
+                    memberDAO.deleteMember();
+                    break;
+                case 5:
+                    System.out.println("프로그램을 종료합니다.");
+                    System.exit(0);
+                default:
+                    System.out.println("유효한 메뉴를 선택하세요.");
+            }
         }
     }
 
-    private class MemberOverload implements memberInterface {
-        @Override
-        public String[][] memberInsert(String id, String name, float height, float weight, int age) {
+    interface MemberDAO {
+        void addMember();
 
-            return new String[0][];
+        void updateMember();
+
+        void getMember();
+
+        void deleteMember();
+    }
+
+    static class MemberDAOImpl implements MemberDAO {
+        private Connection conn;
+
+        public MemberDAOImpl() {
+            try {
+                conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
-        public String[][] memberUpdate(String id, String name, float height, float weight, int age) {
-            return new String[0][];
+        public void addMember() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("*** 회원 추가 ***");
+
+            System.out.print("이름: ");
+            String name = scanner.nextLine();
+
+            System.out.print("나이: ");
+            int age = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("신장: ");
+            float height = scanner.nextFloat();
+
+            System.out.println("아이디: ");
+            String id = scanner.next();
+
+            try {
+                PreparedStatement stmt = conn.prepareStatement("INSERT INTO member (name, age, height, id) VALUES (?, ?, ?, ?)");
+                stmt.setString(1, name);
+                stmt.setInt(2, age);
+                stmt.setFloat(3, height);
+                stmt.setString(4, id);
+                stmt.executeUpdate();
+                System.out.println("회원이 추가되었습니다.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
-        public void memberDelete(String id) {
+        public void updateMember() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("===회원 수정 ===");
 
+            System.out.print("수정할 회원의 ID: ");
+            String id = scanner.next();
+
+            System.out.print("새로운 이름: ");
+            String name = scanner.next();
+
+            System.out.print("새로운 나이: ");
+            int age = scanner.nextInt();
+
+            System.out.print("새로운 신장: ");
+            String height = scanner.next();
+
+            try {
+                PreparedStatement stmt = conn.prepareStatement("UPDATE member SET name = ?, age = ?, height = ? WHERE id = ?");
+                stmt.setString(1, name);
+                stmt.setInt(2, age);
+                stmt.setString(3, height);
+                stmt.setString(4, id);
+                int rowsAffected = stmt.executeUpdate();
+                if (rowsAffected > 0) {
+                    System.out.println("회원이 수정되었습니다.");
+                } else {
+                    System.out.println("해당하는 ID의 회원이 존재하지 않습니다.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
-        public String[][] memberSelectAll() {
-            return new String[0][];
+        public void getMember() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("*** 회원 조회 ***");
+
+            System.out.print("조회할 회원의 ID: ");
+            String id = scanner.next();
+
+            try {
+                PreparedStatement stmt = conn.prepareStatement("SELECT * FROM member WHERE id = ?");
+                stmt.setString(1, id);
+                ResultSet rs = stmt.executeQuery();
+                if (rs.next()) {
+                    System.out.println("ID: " + rs.getInt("id"));
+                    System.out.println("이름: " + rs.getString("name"));
+                    System.out.println("나이: " + rs.getInt("age"));
+                    System.out.println("신장: " + rs.getFloat("height"));
+                } else {
+                    System.out.println("해당하는 ID의 회원이 존재하지 않습니다.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void deleteMember() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("*** 회원 삭제 ***");
+
+            System.out.print("삭제할 회원의 ID: ");
+            String id = scanner.next();
+
+            try {
+                PreparedStatement stmt = conn.prepareStatement("DELETE FROM member WHERE id = ?");
+                stmt.setString(1, id);
+                int rowsAffected = stmt.executeUpdate();
+                if (rowsAffected > 0) {
+                    System.out.println("회원이 삭제되었습니다.");
+                } else {
+                    System.out.println("해당하는 ID의 회원이 존재하지 않습니다.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
